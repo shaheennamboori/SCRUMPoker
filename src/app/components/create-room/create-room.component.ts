9ec9b96a0583e78f9731b3075d6b40fbf9f3c815
Shaheen N <chichushaheen@gmail.com>
2024-12-29 11:54:44 +0530

    created glass box css , changed light theme background

--- src/app/components/create-room/create-room.component.ts
+++ src/app/components/create-room/create-room.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-room',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-room.component.html',
   styleUrl: './create-room.component.scss'
 })
 export class CreateRoomComponent {
-
+  public name = new FormControl('');
+  public password = new FormControl('');
 }
