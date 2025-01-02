42e9cda6eb133304759c362006e0438c1b690a22
Shaheen N <chichushaheen@gmail.com>
2025-01-02 13:03:24 +0530

    short notation for styles and variables import

--- src/app/components/room-list/room-list.component.ts
+++ src/app/components/room-list/room-list.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-room-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './room-list.component.html',
   styleUrl: './room-list.component.scss'
 })
 export class RoomListComponent {
-
+  public roomList: string[] = ["Room1", "Room2", "Room3", "Room4", "Room5"];
+  
 }
